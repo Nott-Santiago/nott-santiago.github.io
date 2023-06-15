88af81d8e89897158a81379fe8cb63b67ef319f7
Nott-Santiago <83885864+Nott-Santiago@users.noreply.github.com>
2023-06-15 15:00:36 -0600

    api-v1
    Se puede consultar la imagen del dia de la api de la nasa

---
+++ app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load',getDatos);
+
+async function getDatos(){
+    const nasa_clave = 'z9uSiKxjggAVhfeWWwhv9JNOA9hXvtX1Qcah6vwg';
+    const nasa_ruta =  `https://api.nasa.gov/planetary/apod?api_key=${nasa_clave}`;
+
+    try {
+        const response = await fetch(nasa_ruta);
+        const results = await response.json();
+        showResultados(results);
+        console.log(results);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    
+}
+
+function showResultados({title, media_type, url, explanation, date}){
+    const titulo = document.querySelector('#titulo');
+    titulo.innerHTML = 'W'+title;
+    
+    const multimedia = document.querySelector('#multimedia');
+    if(media_type == 'video'){
+        multimedia.innerHTML = `
+        <iframe class="embed-responsive-item" src="${url}"></iframe`;
+    }else{
+        multimedia.innerHTML = `
+        <img class="img-fluid img-dia" src="${url}" alt="${url}">`;
+    }
+    const explicacion = document.querySelector('#explicacion');
+    explicacion.innerHTML = explanation;
+
+    const fecha = document.querySelector('#fecha');
+    const fechaObtenida = new Date(date);
+    const diaSemana = fechaObtenida.toLocaleString('es-ES', { weekday: 'long' });
+    const dia = fechaObtenida.getDate();
+    const mes = fechaObtenida.toLocaleString('es-ES', { month: 'long' });
+    const anio = fechaObtenida.getFullYear();
+    const fechaFormateada = `${diaSemana} ${dia} de ${mes} del ${anio}`;
+    fecha.innerHTML = fechaFormateada;
+}
